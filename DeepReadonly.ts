b941368d9b0b2d9aee9a40ca00450d93eec2803a
Tishida <archersparadox.100@gmail.com>
2024-03-24 12:37:16 +0900

    DeepReadonly

---
+++ DeepReadonly.ts
@@ -0,0 +1,32 @@
+type X = {
+  x: {
+    a: 1;
+    b: "hi";
+  };
+  y: "hey";
+};
+
+type Expected = {
+  readonly x: {
+    readonly a: 1;
+    readonly b: "hi";
+  };
+  readonly y: "hey";
+};
+
+type ExpectedArr = [
+  {
+    readonly x: {
+      readonly a: 1;
+      readonly b: "hi";
+    };
+    readonly y: "hey";
+  }
+];
+
+type DeepReadonly<T> = keyof T extends never
+  ? T
+  : { readonly [k in keyof T]: DeepReadonly<T[k]> };
+
+type DeepReadonlyTodo = DeepReadonly<X>; // should be same as `Expected`
+type DeepReadonlyTodos = DeepReadonly<ExpectedArr>; // should be same as `Expected`
